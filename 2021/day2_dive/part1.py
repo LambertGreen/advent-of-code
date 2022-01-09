cb9435f6d11f227a432c6e6180e3fbc6c0d7d587
Lambert Green <lambert_green@msn.com>
2022-01-08 16:33:51 -0800

    Add solution for Day 2: Dive (Part 1)

---
+++ 2021/day2_dive/part1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return [line.split() for line in f]
+
+def get_position(commands):
+    horizontal = 0
+    depth = 0
+    for command in commands:
+        direction = command[0]
+        distance = int(command[1])
+        # print("{} {}".format(direction, distance))
+
+        if direction == "forward":
+            horizontal += distance
+        elif direction == "down":
+            depth += distance
+        elif direction == "up":
+            depth -= distance
+        # print("{} {}".format(horizontal, depth))
+
+    return horizontal * depth
+
+get_position(read('input.txt'))
