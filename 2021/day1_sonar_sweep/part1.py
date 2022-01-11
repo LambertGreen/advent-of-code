0b8965c1efee245fe991e762ebd08f75d1d5d858
Lambert Green <lambert_green@msn.com>
2022-01-10 16:36:49 -0800

    Ran formatting tool

--- 2021/day1_sonar_sweep/part1.py
+++ 2021/day1_sonar_sweep/part1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 "Solves: Day1: Sonar Sweep"
 
+
 def read(filename):
     return [int(num) for num in open(filename)]
+
 
 def get_increase_count(nums):
     count = 0
@@ -11,5 +13,6 @@ def get_increase_count(nums):
             count += 1
     return count
 
-print("result with sample_input: {}".format(get_increase_count(read('sample_input.txt'))))
-print("result with input: {}".format(get_increase_count(read('input.txt'))))
+
+print("sample_input: {}".format(get_increase_count(read("sample_input.txt"))))
+print("input: {}".format(get_increase_count(read("input.txt"))))
--- 2021/day1_sonar_sweep/part2.py
+++ 2021/day1_sonar_sweep/part2.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 "Solves: Day1: Sonar Sweep"
 
+
 def read(filename):
     return [int(num) for num in open(filename)]
+
 
 def get_increase_count(nums):
     window_size = 3
     count = 0
-    prev_window_sum = 0;
+    prev_window_sum = 0
     for i in range(window_size):
         prev_window_sum += nums[i]
-
-    print("{} -  no previous sum".format(prev_window_sum))
 
     for i in range(window_size, len(nums)):
         curr_window_sum = prev_window_sum - nums[i - window_size] + nums[i]
         if curr_window_sum > prev_window_sum:
             count += 1
-            print("{} - increased".format(curr_window_sum))
-        else:
-            print("{} - decreased".format(curr_window_sum))
         prev_window_sum = curr_window_sum
     return count
 
-print("result with sample_input: {}".format(get_increase_count(read('sample_input.txt'))))
-print("result with input: {}".format(get_increase_count(read('input.txt'))))
+
+print("sample_input: {}".format(get_increase_count(read("sample_input.txt"))))
+print("input: {}".format(get_increase_count(read("input.txt"))))
